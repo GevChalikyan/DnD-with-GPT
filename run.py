110dc204dd93000bc7e68b5c73eb957dd2681706
GevorkChalikyan <72428068+GevorkChalikyan@users.noreply.github.com>
2023-05-05 22:01:10 -0700

    First commit

---
+++ run.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import openai
+
+def collectAPIKey():
+    APIKey = input('Insert your API Key: ')
+    with open('API Key.txt', 'w') as f:
+        f.write(APIKey)
+
+
+
+#	Checks if the user has given an API key yet
+#	Collects one if there isn't one
+try:
+    my_abs_path = Path("./API Key.txt").resolve(strict=True)
+except FileNotFoundError:
+    collectAPIKey()
+with open ('API Key.txt') as f:
+    openai.api_key = f.readline()
+os.system('clear')
+
+
+
+messages = [
+    {
+        "role": "system", 
+        "content": "You're the dungeon master of an epic dungeons and dragons campaign! Lead your player through an epic adventure!"
+    }
+]
+
+while True:
+    content = input("User: ")
+    messages.append({"role": "user", "content": content})
+
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(f'\nChatGPT: {chat_response}\n')
